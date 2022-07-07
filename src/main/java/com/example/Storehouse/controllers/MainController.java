867c13d26b490f705fa9b73ed562456e7bb4b45c
SergiuA17 <98698995+SergiuLA17@users.noreply.github.com>
2022-07-07 15:33:54 +0300

    rewrote  method using Optional, added v3 of sql file
    added functionality - update product quantity, when it's was purchased

--- src/main/java/com/example/Storehouse/controllers/MainController.java
+++ src/main/java/com/example/Storehouse/controllers/MainController.java
@@ -1,9 +1,10 @@
 package com.example.Storehouse.controllers;
-
 import com.example.Storehouse.entity.Products;
 import com.example.Storehouse.exception.ProductNotfoundException;
 import com.example.Storehouse.model.Product;
 import com.example.Storehouse.service.StorehouseService;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
@@ -15,15 +16,23 @@ import org.springframework.web.bind.annotation.*;
 public class MainController {
     @Autowired
     private StorehouseService storehouseService;
+    private final Logger logger = LoggerFactory.getLogger(MainController.class);
 
     @GetMapping(path = "/getProduct")
-    public ResponseEntity getProduct(@RequestParam("name") String name) {
+    public ResponseEntity getProduct(@RequestParam("name") String name, @RequestParam("quantity") int quantity) {
         Products product = storehouseService.findByName(name);
+        Products productForUpdate = storehouseService.findByName(name);
+        logger.info("Request to purchase " + quantity + " kg of " + name);
 
-        try {
-            return ResponseEntity.ok(Product.toModel(product));
-        } catch (Exception e) {
-            throw new ProductNotfoundException();
-        }
+        if (storehouseService.checkQuantityProduct(product, quantity)) {
+            storehouseService.updateProductWhenPurchased(productForUpdate, quantity);
+            try {
+                return ResponseEntity.ok(Product.toModel(product, quantity));
+            } catch (Exception e) {
+                throw new ProductNotfoundException();
+            }
+        } else
+            storehouseService.updateProductWhenProductNotFound(product);
+        throw new ProductNotfoundException();
     }
 }
---
+++ src/main/resources/db/migration/V3__add_data.sql
@@ -0,0 +1,7 @@
+INSERT INTO `Storehouse`.`products` (`name`, `quantity`, `date_of_manufacture`, `days_to_expire`)VALUES ('Banana', '400', LOCALTIME, '15');
+INSERT INTO `Storehouse`.`products` (`name`, `quantity`, `date_of_manufacture`, `days_to_expire`)VALUES ('Kiwi', '250', LOCALTIME, '15');
+INSERT INTO `Storehouse`.`products` (`name`, `quantity`, `date_of_manufacture`, `days_to_expire`)VALUES ('Carrot', '375', LOCALTIME, '30');
+INSERT INTO `Storehouse`.`products` (`name`, `quantity`, `date_of_manufacture`, `days_to_expire`)VALUES ('Mango', '195', LOCALTIME, '30');
+INSERT INTO `Storehouse`.`products` (`name`, `quantity`, `date_of_manufacture`, `days_to_expire`)VALUES ('Orange', '447', LOCALTIME, '15');
+INSERT INTO `Storehouse`.`products` (`name`, `quantity`, `date_of_manufacture`, `days_to_expire`)VALUES ('Pear', '567', LOCALTIME, '15');
+INSERT INTO `Storehouse`.`products` (`name`, `quantity`, `date_of_manufacture`, `days_to_expire`)VALUES ('Peach', '743', LOCALTIME, '15');
